90dfeea79f1e0f24e4d561b2cf6d02afa46a5ed9
朱曼丽 <16740085+zhu-manli-stecey@user.noreply.gitee.com>
2026-03-23 17:52:54 +0000

    update vite.config.ts.
    Signed-off-by: 朱曼丽 <16740085+zhu-manli-stecey@user.noreply.gitee.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'  // 添加这行
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {  // 添加这个 resolve 配置
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
