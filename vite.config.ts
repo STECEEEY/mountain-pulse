aeb9a65ac76b13643d8728db07ca6f91d7bd9950
朱曼丽 <16740085+zhu-manli-stecey@user.noreply.gitee.com>
2026-03-24 01:23:09 +0000

    update vite.config.ts.
    Signed-off-by: 朱曼丽 <16740085+zhu-manli-stecey@user.noreply.gitee.com>

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,17 @@ export default defineConfig({
         target: 'http://47.102.147.118:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      // 添加地质云代理
+      '/geology-cloud': {
+        target: 'https://igss.cgs.gov.cn:6160',
+        changeOrigin: true,
+        secure: false, // 如果目标服务器是https但证书有问题，设置为false
+        rewrite: (path) => path.replace(/^\/geology-cloud/, ''),
+        headers: {
+          'Origin': 'https://igss.cgs.gov.cn',
+          'Referer': 'https://igss.cgs.gov.cn'
+        }
       }
     }
   }
