80a5a156b3570cd30ac44c3bdd1a8d4b453dc7b6
STECEEEY <2122855316@qq.com>
2026-04-02 21:10:39 +0800

    Update weatherService.ts

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -40,7 +40,7 @@ class WeatherService {
         const weatherResponse = await axios.get('https://restapi.amap.com/v3/weather/weatherInfo', {
           params: {
             city: adcode,
-            key: 4d95a8667039c3e5bb3ddb03ce1c71b8,
+            key: GAODE_KEY,
             extensions: 'all'
           },
           timeout: 10000
